0d1e47ba432a496886394f5689acbbe598644a57
Batman <124198959+Artyr490@users.noreply.github.com>
2025-11-03 12:03:28 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function lahetaVastaukset() {
+    alert("kukkuus")
+//     var total = 5;
+//     var score = 0;
+//     var huom = 0 
+//     var k1 = document.forms["kyselyLomake"]["k1"].value;
+//     var k2 = document.forms["kyselyLomake"]["k2"].value;
+//     var k3 = document.forms["kyselyLomake"]["k3"].value;
+//     var k4 = document.forms["kyselyLomake"]["k4"].value;
+//     var k5 = document.forms["kyselyLomake"]["k5"].value;
+// for (i = 1; i <= total; i++) {
+//     if (eval('k' + i) == null || eval('k' + i) == '') {
+//         alert('Et vastannut kysymykseen numero: ' + i);
+//  huom=1;
+//     }
+//     if(huom==1){
+//       return false;
+//     }
+// }
+// var vastaukset = ["b", "a", "d", "b", "d"];
+
+// for (i = 1; i <= total; i++) {
+//     if (eval('k' + i) == vastaukset[i - 1]) {
+//         score++;
+//     }
+// }
+// var tulokset = document.getElementById('tulokset');
+// alert('Sait ' + score + ' pistettä, kun maksimi pistemäärä oli ' + total); 
+// tulokset.innerHTML = '<h3> Sait  <span>' + pisteet + '</span> pistettä, kun maksimi pistemäärä oli <span> ' + yhteensa + ' </span></h3>';
+// return false;
+}
